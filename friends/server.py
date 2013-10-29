b31021285552ceda81d0e00b90fb027e7181de4e
Mikael Frykholm <mikael@frykholm.com>
2013-10-29 20:55:08 +0100

    webfinger mockup
    webfinger mockup

---
+++ friends/server.py
@@ -0,0 +1,28 @@
+import tornado.ioloop
+import tornado.web
+import os
+import tornado.httpserver
+class XrdHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("templates/xrd.xml", hostname="ronin.local", url="https://ronin.local")
+
+class UserHandler(tornado.web.RequestHandler):
+    def get(self, user):
+        #user = self.get_argument("user")
+        self.render("templates/user.xml", user=user)
+
+application = tornado.web.Application([
+    (r"/.well-known/host-meta", XrdHandler),
+    (r"/user/(.+)", UserHandler),
+    ],debug=True,
+    
+)
+srv = tornado.httpserver.HTTPServer(application, ssl_options={
+        "certfile":  "ronin.local.pem",
+        "keyfile":  "ronin.local.key",
+    })
+
+
+if __name__ == "__main__":
+    srv.listen(443)
+    tornado.ioloop.IOLoop.instance().start()
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='friends',
+    version='0.1.0',
+    author='Mikael Frykholm',
+    author_email='mikael@frykholm.com',
+    packages=['friends'],
+    
+    url='https://github.com/mikaelfrykholm/friends/',
+    license='LICENSE.txt',
+    description='Ostatus app.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "tornadoweb >= 3.1",
+    ],
+)
